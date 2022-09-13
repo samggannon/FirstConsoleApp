e698367eb0a9fc5354c3e391fd89f32325be1ec8
Samuel Gannon <samgannon@rocketmail.com>
2022-09-13 11:04:49 -0500

    Now we have many messages

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
+
