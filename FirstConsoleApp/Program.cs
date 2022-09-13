9a5d2443e9a7baddb7e2ed0670334f00c9558a96
Samuel Gannon <samgannon@rocketmail.com>
2022-09-13 13:00:03 -0500

    Revert "keeping only one message"
    This reverts commit 71043fd2dc227bea48e9cea8706c9282d10d1fbf.

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy!");
-
+Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
+Console.WriteLine("Hello, C# Academy!");
 
